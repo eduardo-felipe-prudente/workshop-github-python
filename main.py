61847ae2c77d3cfdc771b481fad129d82eed61f9
eduardo-felipe-prudente <eduardofelipe.240999@gmail.com>
2023-06-06 21:42:56 -0300

    altera a mensagem do print

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ name = input('digite seu nome')
 city = input('digite a cidade ')
 gender = input('digite o sexo ')
 
-print('seus dados sao: ')
+print('seguem os dados: ')
 print(name, ' - ', city, ' - ', gender)
